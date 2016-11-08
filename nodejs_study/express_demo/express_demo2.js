6b22a5aabb14a9115bdb5444974248a36ef63af2
wujunwei <wujunwei@le.com>
2016-11-08 14:46:18 +0800

    删除不必要的文件

--- nodejs_study/express_demo/express_demo2.js
+++ nodejs_study/express_demo/express_demo2.js
@@ -3,8 +3,7 @@ var express = require('express');
 var app = express();
 
 //  主页输出 "Hello World"
-app.get('/', function (req, res) {
-   console.log("主页 GET 请求");
+app.get('/', function (req, res) {   console.log("主页 GET 请求");
    res.send('Hello GET');
 })
 
